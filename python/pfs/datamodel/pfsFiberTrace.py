596ee52b1cca4249aa2c07b1b0c11a65c8222ed6
Andreas Ritter <azuri.ritter@gmail.com>
2017-08-09 13:21:51 -0400

    Fix the pfsModel files for PIPE2D-235
    N.b. See also DAMD-20

--- python/pfs/datamodel/pfsFiberTrace.py
+++ python/pfs/datamodel/pfsFiberTrace.py
@@ -6,6 +6,10 @@ try:
 except ImportError:
     pyfits = None
 
+import lsst.afw.geom as afwGeom
+import lsst.afw.image as afwImage
+import lsst.daf.base as dafBase
+
 class PfsFiberTrace(object):
     """A class corresponding to a single fiberTrace file"""
     fileNameFormat = "pfsFiberTrace-%10s-0-%1s%1d.fits"
@@ -15,38 +19,8 @@ class PfsFiberTrace(object):
         self.spectrograph = spectrograph
         self.arm = arm
 
-        self.fwhm = 0.
-        self.threshold = 0.
-        self.nTerms = 0
-        self.saturationLevel = 0.
-        self.minLength = 0
-        self.maxLength = 0
-        self.nLost = 0
-        self.traceFunction = 'POLYNOMIAL'
-        self.order = 0
-        self.xLow = 0.
-        self.xHigh = 0.
-        self.nCutLeft = 0
-        self.nCutRight = 0
-        self.interpol = 'SPLINE3'
-#        READNOISE WHY????
-        self.swathLength = 0
-        self.overSample = 0
-        self.maxIterSF = 0
-        self.maxIterSig = 0
-        self.lambdaSF = 0.
-        self.lambdaSP = 0.
-        self.lambdaWing = 0.
-        self.lSigma = 0.
-        self.uSigma = 0.
-
         self.fiberId = []
-        self.xCenter = []
-        self.yCenter = []
-        self.yLow = []
-        self.yHigh = []
-        self.coeffs = []
-        self.profiles = []
+        self.traces = []
 
     def read(self, dirName="."):
         """Read self's pfsFiberTrace file from directory dirName"""
@@ -54,111 +28,91 @@ class PfsFiberTrace(object):
             raise RuntimeError("I failed to import pyfits, so cannot read from disk")
 
         fileName = PfsFiberTrace.fileNameFormat % (self.obsDate, self.arm, self.spectrograph)
-        fd = pyfits.open(os.path.join(dirName, fileName)) 
-        
-        prihdr = fd[0].header
-        self.fwhm = prihdr['FWHM']
-        self.threshold = prihdr['THRESH']
-        self.nTerms = prihdr['NTERMS']
-        self.saturationLevel = prihdr['SATLEVEL']
-        self.minLength = prihdr['MINLEN']
-        self.maxLength = prihdr['MAXLEN']
-        self.nLost = prihdr['NLOST']
-        self.traceFunction = prihdr['FUNC']
-        self.order = prihdr['ORDER']
-        self.xLow = prihdr['XLOW']
-        self.xHigh = prihdr['XHIGH']
-        self.nCutLeft = prihdr['NCUTL']
-        self.nCutRight = prihdr['NCUTR']
-        self.interpol = prihdr['INTERPOL']
-#        READNOISE WHY????
-        self.swathLength = prihdr['SWATHLEN']
-        self.overSample = prihdr['OSAMPLE']
-        self.maxIterSF = prihdr['MAXITSF']
-        self.maxIterSig = prihdr['MAXITSIG']
-        self.lambdaSF = prihdr['LAMBDASF']
-        self.lambdaSP = prihdr['LAMBDASP']
-        self.lambdaWing = prihdr['LAMBWING']
-        self.lSigma = prihdr['LSIGMA']
-        self.uSigma = prihdr['USIGMA']
 
-        for hduName in ["FUNCTION", "PROFILE"]:
-            hdu = fd[hduName]
-            hdr, data = hdu.header, hdu.data
+        allTracesMI = afwImage.MaskedImageF(os.path.join(dirName, fileName))
+        with pyfits.open(os.path.join(dirName, fileName)) as fd:
+            hdu = fd["ID_BOX"]
+            self.fiberId = hdu.data['FIBERID']
+            minX = hdu.data['MINX']
+            minY = hdu.data['MINY']
+            maxX = hdu.data['MAXX']
+            maxY = hdu.data['MAXY']
 
-            if hduName == "FUNCTION":
-                self.fiberId = data['FIBERID']
-                self.xCenter = data['XCENTER']
-                self.yCenter = data['YCENTER']
-                self.yLow = data['YLOW']
-                self.yHigh = data['YHIGH']
-                self.coeffs = data['COEFFS']
-            elif hduName == "PROFILE":
-                self.profiles = data
-            else:
-                raise RuntimeError("Unexpected HDU %s reading %s" % (hduName, fileName))
+        self.traces = []
+        x0 = 0
+        for i in range(len(self.fiberId)):
+            # bbox: BBox in full (i.e. data) image
+            bbox = afwGeom.BoxI(afwGeom.PointI(minX[i], minY[i]), afwGeom.PointI(maxX[i], maxY[i]))
+
+            # bboxAllTMI: BBox in allTracesMI
+            bboxAllTMI = afwGeom.BoxI(afwGeom.PointI(x0, bbox.getMinY()), bbox.getDimensions())
+            
+            trace = allTracesMI[bboxAllTMI].clone()
+            trace.setXY0(bbox.getBegin())
+            
+            self.traces.append(trace)
+            x0 += bbox.getWidth()
 
     def write(self, dirName=".", fileName=None):
         if not pyfits:
             raise RuntimeError("I failed to import pyfits, so cannot read from disk")
 
-        hdus = pyfits.HDUList()
-
-        hdr = pyfits.Header()
-        hdr['OBSTYPE'] = "FIBERTRACE"
-        hdr['FWHM'] = self.fwhm
-        hdr['THRESH'] = self.threshold
-        hdr['NTERMS'] = self.nTerms
-        hdr['SATLEVEL'] = self.saturationLevel
-        hdr['MINLEN'] = self.minLength
-        hdr['MAXLEN'] = self.maxLength
-        hdr['NLOST'] = self.nLost
-        hdr['FUNC'] = self.traceFunction
-        hdr['ORDER'] = self.order
-        hdr['XLOW'] = self.xLow
-        hdr['XHIGH'] = self.xHigh
-        hdr['NCUTL'] = self.nCutLeft
-        hdr['NCUTR'] = self.nCutRight
-        hdr['INTERPOL'] = self.interpol
-#        READNOISE WHY????
-        hdr['SWATHLEN'] = self.swathLength
-        hdr['OSAMPLE'] = self.overSample
-        hdr['MAXITSF'] = self.maxIterSF
-        hdr['MAXITSIG'] = self.maxIterSig
-        hdr['LAMBDASF'] = self.lambdaSF
-        hdr['LAMBDASP'] = self.lambdaSP
-        hdr['LAMBWING'] = self.lambdaWing
-        hdr['LSIGMA'] = self.lSigma
-        hdr['USIGMA'] = self.uSigma
-        hdr.update()
-        hdus.append(pyfits.PrimaryHDU(header=hdr))
-
-        coeffArr = np.array(self.coeffs, dtype=np.float32)
-        hdu = pyfits.BinTableHDU.from_columns([
-            pyfits.Column(name = 'FIBERID', format = 'J',
-                          array=np.array(self.fiberId, dtype=np.int32)),
-            pyfits.Column(name = 'XCENTER', format = 'E',
-                          array=np.array(self.xCenter, dtype=np.float32)),
-            pyfits.Column(name = 'YCENTER', format = 'J',
-                          array=np.array(self.yCenter, dtype=np.int32)),
-            pyfits.Column(name = 'YLOW', format = 'J',
-                          array=np.array(self.yLow, dtype=np.int32)),
-            pyfits.Column(name = 'YHIGH', format = 'J',
-                          array=np.array(self.yHigh, dtype=np.int32)),
-            pyfits.Column(name = 'COEFFS', format = '%dE'%(self.order+1),
-                          array=coeffArr)
-        ])
-        hdu.name = 'FUNCTION'
-        hdu.header["INHERIT"] = True
-        hdus.append(hdu)
-
-        hdu = pyfits.ImageHDU(self.profiles)
-        hdu.name = "PROFILE"
-        hdu.header["INHERIT"] = True
-        hdus.append(hdu)
+        hdr = dafBase.PropertySet()
+        hdr.add('OBSTYPE', 'FIBERTRACE')
 
         # clobber=True in writeto prints a message, so use open instead
         if fileName is None:
             fileName = self.fileNameFormat % (self.obsDate, self.arm, self.spectrograph)
-        with open(os.path.join(dirName, fileName), "w") as fd:
-            hdus.writeto(fd)
+        fullFileName = os.path.join(dirName, fileName)
+        #
+        # We'll pack all the traces into a single masked image, so figure out how large it needs to be
+        #
+        # Start by unpacking the traces' BBoxes; we need to do this anyway for the fits I/O
+        #
+        minX = []
+        minY = []
+        maxX = []
+        maxY = []
+        width = 0
+        for i in range(len(self.traces)):
+            bbox = self.traces[i].getBBox()
+
+            minX.append(bbox.getMinX())
+            minY.append(bbox.getMinY())
+            maxX.append(bbox.getMaxX())
+            maxY.append(bbox.getMaxY())
+
+            width += bbox.getWidth()
+
+        height = max(maxY) + 1
+        allTracesMI = afwImage.MaskedImageF(width, height)
+
+        # Copy trace's MaskedImages to allTracesMI
+        x0 = 0
+        origin = afwGeom.PointI(0, 0)
+        for i in range(len(self.traces)):
+            trace = self.traces[i]
+
+            xy0 = afwGeom.Point2I(x0, minY[i]) # origin in allTracesMI
+            allTracesMI[afwGeom.BoxI(xy0, trace.getDimensions())] = \
+                        trace.Factory(trace, afwGeom.BoxI(origin, trace.getDimensions()), afwImage.LOCAL)
+
+            x0 += trace.getWidth()
+
+        hdu = pyfits.BinTableHDU.from_columns([
+            pyfits.Column(name = 'FIBERID', format = 'J', array=np.array(self.fiberId, dtype=np.int32)),
+            pyfits.Column(name = 'MINX', format = 'J', array=np.array(minX, dtype=np.int32)),
+            pyfits.Column(name = 'MINY', format = 'J', array=np.array(minY, dtype=np.int32)),
+            pyfits.Column(name = 'MAXX', format = 'J', array=np.array(maxX, dtype=np.int32)),
+            pyfits.Column(name = 'MAXY', format = 'J', array=np.array(maxY, dtype=np.int32)),
+        ])
+        hdu.name = "ID_BOX"
+        hdu.header["INHERIT"] = True
+
+        # Write fits file from MaskedImage and then append the additional HDUs
+        allTracesMI.writeFits(fullFileName, hdr)
+        with pyfits.open(fullFileName, "update") as fd:
+            fd[1].name = "IMAGE"
+            fd[2].name = "MASK"
+            fd[3].name = "VARIANCE"
+            fd.append(hdu)
