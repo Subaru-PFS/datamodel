ecd5a6de29e2ad912c3cb395778bade8fcde23b3
Hassan Siddiqui <hassans@astro.princeton.edu>
2019-06-27 09:18:44 -0400

    Changed positioning of catId in datamodel formats in datamodel.txt
    The catId component was agreed to be repositioned as the first for
    search performance purposes.
    Unit tests have been added to verify the fileformats.

---
+++ tests/test_FileFormats.py
@@ -0,0 +1,83 @@
+import unittest
+from pfs.datamodel.drp import PfsObject
+from pfs.datamodel.drp import PfsReference
+from pfs.datamodel.drp import PfsMerged
+from pfs.datamodel.drp import PfsSingle
+from pfs.datamodel.drp import PfsArm
+import re
+
+
+class FileFormatTestCase(unittest.TestCase):
+    """ Checks the format of example datamodel files are
+        consistent with that specified in the corresponding
+        datamodel classes.
+    """
+
+    def extractAttributes(self, cls, fileName):
+        matches = re.search(cls.filenameRegex, fileName)
+        if not matches:
+            self.fail(
+                "Unable to parse filename: {} using regex {}"
+                .format(fileName, cls.filenameRegex))
+
+        # Cannot use algorithm in PfsSpectra._parseFilename(),
+        # specifically cls.filenameKeys, due to ambiguity in parsing
+        # integers in hex format (eg objId). Need to parse cls.filenameFormat
+        ff = re.search(r'^[a-zA-Z]+(.*)\.fits', cls.filenameFormat)[1]
+        cmps = re.findall(r'-{0,1}(0x){0,1}\%\((\w+)\)\d*(\w)', ff)
+        fmts = [(kk, tt) for ox, kk, tt in cmps]
+
+        d = {}
+        for (kk, tt), vv in zip(fmts, matches.groups()):
+            if tt == 'd':
+                ii = int(vv)
+            elif tt == 'x':
+                ii = int(vv, 16)
+            elif tt == 's':
+                ii = vv
+            d[kk] = ii
+        return d
+
+    @unittest.expectedFailure
+    def testBadHashInFileName(self):
+        self.extractAttributes(
+            PfsObject,
+            'pfsObject-07621-2,2-001-02468ace1234abcd-003-1234abcddeadbeef.fits')
+
+    def testPfsArm(self):
+        d = self.extractAttributes(PfsArm, 'pfsArm-123450-b1.fits')
+        self.assertEqual(d['visit'], 123450)
+        self.assertEqual(d['arm'], 'b')
+        self.assertEqual(d['spectrograph'], 1)
+
+    def testPfsMerged(self):
+        d = self.extractAttributes(PfsMerged, 'pfsMerged-012345.fits')
+        self.assertEqual(d['visit'], 12345)
+
+    def testPfsReference(self):
+        d = self.extractAttributes(
+            PfsReference, 'pfsReference-001-07621-2,2-02468ace1234abcd.fits')
+        self.assertEqual(d['tract'], 7621)
+        self.assertEqual(d['patch'], '2,2')
+        self.assertEqual(d['catId'], 1)
+        self.assertEqual(d['objId'], 0x02468ace1234abcd)
+
+    def testPfsSingle(self):
+        d = self.extractAttributes(
+            PfsSingle, 'pfsSingle-123-76210-2,2-02468ace1234abcd-123450.fits')
+        self.assertEqual(d['tract'], 76210)
+        self.assertEqual(d['patch'], '2,2')
+        self.assertEqual(d['catId'], 123)
+        self.assertEqual(d['objId'], 0x02468ace1234abcd)
+        self.assertEqual(d['visit'], 123450)
+
+    def testPfsObject(self):
+        d = self.extractAttributes(
+            PfsObject,
+            'pfsObject-001-07621-2,2-02468ace1234abcd-003-0x1234abcddeadbeef.fits')
+        self.assertEqual(d['tract'], 7621)
+        self.assertEqual(d['patch'], '2,2')
+        self.assertEqual(d['objId'], 0x02468ace1234abcd)
+        self.assertEqual(d['catId'], 1)
+        self.assertEqual(d['nVisit'], 3)
+        self.assertEqual(d['pfsVisitHash'], 0x1234abcddeadbeef)
