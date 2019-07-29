e02db78383d96d4eda5b62bcda8cef2792298205
Paul Price <price@astro.princeton.edu>
2019-07-29 15:43:42 -0400

    PfsSpectrum: add resample method
    For convenience to the user.

--- python/pfs/datamodel/pfsSpectrum.py
+++ python/pfs/datamodel/pfsSpectrum.py
@@ -6,6 +6,7 @@ from .masks import MaskHelper
 from .target import TargetData, TargetObservations
 from .utils import astropyHeaderFromDict, wraparoundNVisit
 from .fluxTable import FluxTable
+from .interpolate import interpolateFlux, interpolateMask
 
 __all__ = ["PfsSimpleSpectrum", "PfsSpectrum"]
 
@@ -213,6 +214,23 @@ class PfsSimpleSpectrum:
             figure.show()
         return figure, axes
 
+    def resample(self, wavelength):
+        """Resampled the spectrum in wavelength
+
+        Parameters
+        ----------
+        wavelength : `numpy.ndarray` of `float`
+            Desired wavelength sampling.
+
+        Returns
+        -------
+        resampled : `PfsSimpleSpectrum`
+            Resampled spectrum.
+        """
+        flux = interpolateFlux(self.wavelength, self.flux, wavelength)
+        mask = interpolateMask(self.wavelength, self.mask, wavelength)
+        return type(self)(self.target, wavelength, flux, mask, self.flags)
+
 
 class PfsSpectrum(PfsSimpleSpectrum):
     """Spectrum for a single object
@@ -370,3 +388,24 @@ class PfsSpectrum(PfsSimpleSpectrum):
         if show:
             figure.show()
         return figure, axes
+
+    def resample(self, wavelength):
+        """Resampled the spectrum in wavelength
+
+        Parameters
+        ----------
+        wavelength : `numpy.ndarray` of `float`
+            Desired wavelength sampling.
+
+        Returns
+        -------
+        resampled : `PfsSpectrum`
+            Resampled spectrum.
+        """
+        flux = interpolateFlux(self.wavelength, self.flux, wavelength)
+        mask = interpolateMask(self.wavelength, self.mask, wavelength)
+        sky = interpolateFlux(self.wavelength, self.sky, wavelength)
+        covar = np.array([interpolateFlux(self.wavelength, cc, wavelength) for cc in self.covar])
+        covar2 = np.array([[0]])  # Not sure what to put here
+        return type(self)(self.target, self.observations, wavelength, flux, mask, sky, covar, covar2,
+                          self.flags, self.fluxTable)
