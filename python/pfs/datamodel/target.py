427b0bac79a525e9140c49a82c6f46593a08a7cc
Hassan Siddiqui <hassans@astro.princeton.edu>
2020-09-23 16:51:53 -0400

    Changed fiberMag[s] to fiberFlux
    Rename fiberMag AND fiberMags variables to fiberFlux.
    Also changed corresponding comments/docString mentioning
    'magnitude' to 'flux'.

--- python/pfs/datamodel/target.py
+++ python/pfs/datamodel/target.py
@@ -26,13 +26,13 @@ class Target(types.SimpleNamespace):
         Declination of the object.
     targetType : `TargetType`, optional
         Type of target (typically ``SCIENCE``).
-    fiberMags : `dict` mapping `str` to `float`, optional
-        Filter names and corresponding fiber magnitudes.
+    fiberFlux : `dict` mapping `str` to `float`, optional
+        Filter names and corresponding fiber fluxes.
     """
     _attributes = ("catId", "tract", "patch", "objId", "ra", "dec", "targetType")  # Read from header
     """Attributes to read from FITS header (iterable of `str`)"""
 
-    def __init__(self, catId, tract, patch, objId, ra=np.nan, dec=np.nan, targetType=-1, fiberMags=None):
+    def __init__(self, catId, tract, patch, objId, ra=np.nan, dec=np.nan, targetType=-1, fiberFlux=None):
         self.catId = catId
         self.tract = tract
         self.patch = patch
@@ -40,7 +40,7 @@ class Target(types.SimpleNamespace):
         self.ra = ra
         self.dec = dec
         self.targetType = targetType
-        self.fiberMags = fiberMags if fiberMags is not None else {}
+        self.fiberFlux = fiberFlux if fiberFlux is not None else {}
         self.identity = dict(catId=catId, tract=tract, patch=patch, objId=objId)
 
     def __str__(self):
@@ -66,7 +66,7 @@ class Target(types.SimpleNamespace):
         kwargs = {}
         for attr in cls._attributes:
             kwargs[attr] = header[attr.upper()]
-        kwargs["fiberMags"] = dict(zip(hdu.data["filterName"], hdu.data["fiberMag"]))
+        kwargs["fiberFlux"] = dict(zip(hdu.data["filterName"], hdu.data["fiberFlux"]))
         return cls(**kwargs)
 
     def toFits(self, fits):
@@ -78,12 +78,12 @@ class Target(types.SimpleNamespace):
             Opened FITS file.
         """
         from astropy.io.fits import BinTableHDU, Column
-        maxLength = max(len(ff) for ff in self.fiberMags.keys()) if self.fiberMags else 1
+        maxLength = max(len(ff) for ff in self.fiberFlux.keys()) if self.fiberFlux else 1
         header = astropyHeaderFromDict({attr.upper(): getattr(self, attr) for attr in self._attributes})
         header.update(TargetType.getFitsHeaders())
         hdu = BinTableHDU.from_columns([
-            Column("filterName", "%dA" % maxLength, array=list(self.fiberMags.keys())),
-            Column("fiberMag", "E", array=np.array(list(self.fiberMags.values()))),
+            Column("filterName", "%dA" % maxLength, array=list(self.fiberFlux.keys())),
+            Column("fiberFlux", "E", array=np.array(list(self.fiberFlux.values()))),
         ], header=header, name="TARGET")
         fits.append(hdu)
 
@@ -112,11 +112,11 @@ class Target(types.SimpleNamespace):
         objId = pfsConfig.objId[index]
         ra = pfsConfig.ra[index]
         dec = pfsConfig.dec[index]
-        fiberMags = dict(zip(pfsConfig.filterNames[index], pfsConfig.fiberMag[index]))
+        fiberFlux = dict(zip(pfsConfig.filterNames[index], pfsConfig.fiberFlux[index]))
         targetType = pfsConfig.targetType[index]
-        return cls(catId, tract, patch, objId, ra, dec, targetType, fiberMags)
+        return cls(catId, tract, patch, objId, ra, dec, targetType, fiberFlux)
 
     def __reduce__(self):
         """How to pickle"""
         return type(self), (self.catId, self.tract, self.patch, self.objId, self.ra, self.dec,
-                            self.targetType, self.fiberMags)
+                            self.targetType, self.fiberFlux)
