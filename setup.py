df4974d4f716ec8a08882dc218d151d7c55d56b9
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-03-15 16:09:00 +0100

    flake8 correctness

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name="pfs.datamodel",
-      #version="x.y",
+      # version="x.y",
       author="",
-      #author_email="",
-      #description="",
+      # author_email="",
+      # description="",
       url="https://github.com/Subaru-PFS/datamodel/",
       packages=find_packages("python"),
-      package_dir={'':'python'},
+      package_dir={'': 'python'},
       zip_safe=True,
       license="",
       install_requires=["numpy"],
