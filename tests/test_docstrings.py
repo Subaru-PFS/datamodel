3b81f298acd45d2aeebe206a25a4ba82bdf209f4
Laszlo Dobos <dobos@jhu.edu>
2024-05-31 18:43:18 -0400

    Do not fail on members defining class type of other members.

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -5,18 +5,20 @@ import inspect
 import lsst.utils.tests
 
 from pfs.datamodel.drp import PfsArm, PfsMerged, PfsReference, PfsSingle, PfsObject
+from pfs.datamodel.ga import PfsGAObject
 
 
 class DocstringsTestCase(unittest.TestCase):
     def testDocstrings(self):
-        for cls in (PfsArm, PfsMerged, PfsReference, PfsSingle, PfsObject):
+        for cls in (PfsArm, PfsMerged, PfsReference, PfsSingle, PfsObject, PfsGAObject):
             for name, attr in inspect.getmembers(cls):
                 if not hasattr(attr, "__doc__") or not attr.__doc__:
                     continue
                 docstring = attr.__doc__
                 for base in cls.__mro__[1:-1]:
-                    self.assertNotIn(base.__name__, docstring,
-                                     f"{cls.__name__}.{name}.__doc__ contains {base.__name__}: {docstring}")
+                    if not name.endswith("Class"):
+                        self.assertNotIn(base.__name__, docstring,
+                                         f"{cls.__name__}.{name}.__doc__ contains {base.__name__}: {docstring}")
 
 
 class TestMemory(lsst.utils.tests.MemoryTestCase):
