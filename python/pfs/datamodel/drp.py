fe988a1230a17d77f43e74f7582146fa36e3479e
Paul Price <price@astro.princeton.edu>
2024-06-26 17:00:18 -0400

    make blackSpotDistance definition more explicit
    It's not the distance to the edge of the spot, but to the center.

--- python/pfs/datamodel/drp.py
+++ python/pfs/datamodel/drp.py
@@ -27,7 +27,7 @@ class PfsArmNotes(PfsTable):
     fitsExtName = "NOTES"
     schema = [
         Column("blackSpotId", np.int32, "ID of black spot", -1),
-        Column("blackSpotDistance", np.float32, "Distance to nearest black spot (mm)", np.nan),
+        Column("blackSpotDistance", np.float32, "Distance to nearest black spot center (mm)", np.nan),
         Column("blackSpotCorrection", np.float32, "Black spot flux correction", np.nan),
         Column("barycentricCorrection", np.float32, "Barycentric correction applied (km/s)", np.nan),
     ]
@@ -51,7 +51,7 @@ class PfsMergedNotes(PfsTable):
     fitsExtName = "NOTES"
     schema = [
         Column("blackSpotId", np.int32, "ID of black spot", -1),
-        Column("blackSpotDistance", np.float32, "Distance to nearest black spot (mm)", np.nan),
+        Column("blackSpotDistance", np.float32, "Distance to nearest black spot center (mm)", np.nan),
         Column("blackSpotCorrection", np.float32, "Black spot flux correction", np.nan),
         Column("barycentricCorrection", np.float32, "Barycentric correction applied (km/s)", np.nan),
     ]
